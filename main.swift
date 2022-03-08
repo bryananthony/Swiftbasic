705a812f5d3c753729c9ac20e4082ecc7e6767d5
Bryan24 <>
2022-03-08 01:47:32 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,114 @@
+// Constants, Variables, Data Type
+
+var largestNumber: Int
+
+let number1 = 40
+let number2 = 5
+
+if number1 > number2 {
+  largestNumber = number1
+} else {
+  largestNumber = number2
+}
+
+largestNumber = (number1 > number 2) ? number 1 : number2
+
+print(largestNumber)
+
+
+//var largestNumber: Int
+
+//let number1 = 40
+//let number2 = 5
+
+//if number1 > number2 {
+ // largestNumber = number1
+//} else {
+ // largestNumber = number2
+//}
+
+//largestNumber = (number1 > number 2) ? number 1 : number2
+
+//print(largestNumber)            
+
+
+
+//let letter: Character = "u"
+
+//switch letter {
+//  case "a", "i", "u", "e", "o";
+  //   print("the letter is a vowel")
+//  default: 
+ // print("the letter is a consonant")
+//}
+
+
+//let distance: Int = 25
+
+//switch distance {
+// case 0...9:
+ // print("your destination is close")
+ // case 10...99:
+ // print("your destination is not that far")
+ // case 100...999:
+ // print("your destination is far from here")
+ // default:
+ // print("where are the next destination ?")
+//}
+
+
+// macam macam tipe data integer di swift
+//print(Int8.Max)
+//print(Int16.max)
+//print(Int32.max)
+//printInt.max)
+//print(Int64.max)
+
+
+//var numberOfWheels = 2
+
+//switch numberOfWheels {
+ // case 0:
+   //  print("missing Wheels")
+ // case 1:
+  //  print("Unicycle")
+  //case 2:
+   // print("Bicycle")
+  //default:
+  //  print("That's a lot of Wheels what the Heck")
+//}
+
+
+
+
+//var temperature = 75
+
+ // if temperature >= 65 && temperature <= 75 {
+ // print("temperature is okay")
+                  
+  //} else if temperature < 65 {
+  //  print("it is to cold")
+  //} else {
+   // print("it is too hot")
+  //}
+
+
+//var isRain = false
+//
+//if !isRain {
+    //  print("Bring your umbrella")
+//}
+
+    
+//var temperature = 100 
+//
+//  print(temperature)
+
+ // if temperature >= 100 {
+ // print("the water is
+ //       boiling")
+//} else {
+ // print("the water is not 
+  //      boiling")
+//}
+
